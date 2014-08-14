d7f7ae6517e3942912b5872c715b7c8d9271f802
Allen <allenwebbtx@gmail.com>
2014-08-13 20:35:04 -0500

    Added functionality to load key map and update data instead of inserting from scratch

--- php2django.py
+++ php2django.py
@@ -8,7 +8,8 @@ import sys
 import traceback
 import types
 
-#You should create a local_settings.py file for your django settings
+# You should create a local.py file for your django settings in the djattendance
+# submodule.
 settingsPath = "ap.settings.local"
 
 #Add the djattendance submodule to the search path for Python modules 
@@ -68,9 +69,23 @@ class importTemplate:
             self.keyMap = pickle.load(infile)
     
     def importRow(self,row):
-        #try:
         param = {}
+        pk = None
+        key = None
+        
         print row
+        
+        # Get old primary key and see if it already has a corresponding new key
+        if not self.key is None:
+            if isinstance(self.key,types.FunctionType):
+                key = self.key(row)
+            else:
+                key = row[self.key]
+            if key in self.keyMap:
+                pk = self.keyMap[key]
+        
+        # Use the mapping attributes and functions to convert the query row into
+        # a model instance.
         for prop in self.mapping.__dict__:
             if not ignore.match(prop):
                 var = self.mapping.__dict__[prop]
@@ -78,28 +93,44 @@ class importTemplate:
                     param[prop]=var(self.mapping,row)
                 else:
                     param[prop]=row[var]
+                    
         print param
-        modelInstance = self.model.objects.create(**param)
-        modelInstance.save()
-        if not self.key is None:
-            if isinstance(self.key,types.FunctionType):
-                key = self.key(row)
-            else:
-                key = row[self.key]
-            self.keyMap[key]=modelInstance.pk
+        
+        # If an instance doesn't already exist create a new one and update the
+        # keyMap which stores the primary key relationship between the old
+        # models and the new models
+        if pk is None:
+            modelInstance = self.model.objects.create(**param)
+            modelInstance.save()
+            if not key is None:
+                self.keyMap[key]=modelInstance.pk
+        else:
+            modelInstance = self.model.objects.get(pk=pk)
+            for prop, value in param.iteritems():
+                modelInstance.__dict__[prop]=value
+            modelInstance.save()
+            
         print modelInstance
         #except Exception, exp:
         #    print exp
     
     def doImport(self):
+        # load key map if it exists
+        if os.path.isfile(self.getPickleFileName()):
+            self.loadKeyMap()
+        
+        # Execute the mysql query and process the results
         cur.execute(self.query)
         result = cur.fetchall()
         try:
             for row in result:
+                # apply the row filter to check whether or not to import the row
                 if self.rowFilter(row):
+                    # import the row based on self.mapping
                     self.importRow(row)
-        except Exception:
-            traceback.print_exc()
-            
+        except Exception as e: # Exceptions are caught so the keyMap can be saved
+            self.saveKeyMap()
+            raise e
+        
         self.saveKeyMap()
 
